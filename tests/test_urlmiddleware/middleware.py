5eac5b794d578effb49b18994330e2e49a20d360
Dougal Matthews <dougal85@gmail.com>
2011-11-02 10:15:26 +0000

    Improved the test suite to use more testing middleware class names to avoid the possibility of a false positive. Also added further testing conditions for url patterns to include using a dotted path to prefix the patterns and attempting to use a urls.py that doesn't exist and one with no patterns.

--- tests/test_urlmiddleware/middleware.py
+++ tests/test_urlmiddleware/middleware.py
@@ -5,3 +5,15 @@ class NoOpMiddleWare(object):
 
 class NoOpMiddleWare2(object):
     pass
+
+class NoOpMiddleWare3(object):
+    pass
+
+class NoOpMiddleWare4(object):
+    pass
+
+class NoOpMiddleWare5(object):
+    pass
+
+class NoOpMiddleWare6(object):
+    pass
